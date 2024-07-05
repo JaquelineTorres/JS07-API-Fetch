bf38908a8a2233bd009f2b817b2532c922576153
Jaqueline <jaqueline.torres.perez.unam@gmail.com>
2024-07-05 00:17:19 -0600

    modificacion: se acomodaron las cars y se agrego el Placeholder

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 const url = "https://reqres.in/api/users?delay=3";
 
 const getPerson = async (url) => {
-
+    const loader = document.getElementById('loader');
     try {
+        loader.style.display = 'flex'; // Show loader
         const response = await fetch(url);
         const dataJson = await response.json();
         console.log(dataJson.data);
 
-        const listPeople = dataJson.data.map( x => {
-
+        const listPeople = dataJson.data.map(x => {
             return `
-
-            <div class="card" style="width: 18rem;">
-                <img src="${x.avatar}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="firstName">${x.first_name}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="col-lg-3 col-md-4 col-sm-5">
+                    <div class="card m-2 rounded shadow-sm">
+                        <div class="card-body"><strong>${x.first_name} ${x.last_name}</strong></div>
+                        <img src="${x.avatar}" class="card-img-top" alt="${x.first_name}">
+                        <div class="card-body">${x.id}</div>
+                        <div class="card-body">${x.email}</div>
+                    </div>                
                 </div>
-            </div>
             `;
-
         });
 
-        document.querySelector(".list-container").innerHTML = listPeople.join('');
-
-        
+        document.querySelector("#list-container").innerHTML = listPeople.join("");
 
     } catch (exception) {
         console.error("Error", exception);
 
+    } finally {
+        loader.style.display = 'none'; // Hide loader
     }
-
 };
 
 console.log(getPerson(url));
